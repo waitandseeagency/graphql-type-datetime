7c1c665af18028ed9aa0e8a0da1667d7bb105d97
Johann Pinson <johann.pinson@gmail.com>
2018-06-18 14:43:25 +0200

    feat(global): initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { GraphQLScalarType } from 'graphql'
+import { Kind } from 'graphql/language'
+import moment from 'moment'
+
+export default new GraphQLScalarType({
+  name: 'DateTime',
+  description: 'The `DateTime` scalar represents a date and time following the ISO 8601 standard',
+  serialize(value) {
+    // value sent to the client
+    return moment.utc(value).format()
+  },
+  parseValue(value) {
+    // value from the client
+    return value
+  },
+  parseLiteral(ast) {
+    if (ast.kind !== Kind.STRING) {
+      throw new TypeError(
+        `DateTime cannot represent non string type ${String(ast.value != null ? ast.value : null)}`
+      )
+    }
+    return moment.utc(value).format()
+  },
+})
